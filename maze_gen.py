0b9d2799434a65b749806b603c4c73b645859bd7
Lloyd Mckenzie <lmckenzie09@gmail.com>
2012-02-15 00:53:27 -0700

    center fix

--- maze_gen.py
+++ maze_gen.py
@@ -1,3 +1,7 @@
+#go through maze again. If any node has zero neighbors, add one
+#go through if any node's previous is 
+
+
 from sys import argv, exit
 import random
 from collections import defaultdict 
@@ -38,22 +42,26 @@ def make_goal(graph, neighbors, size):
 #Figures out which corner of the center was picked from the list choices.
 #Whichever node was chosen is what opens to the rest of the maze
   if graph[(i-1, j)][1] != True:
-    if graph[(i, j+1)] != True:
+    if graph[(i, j+1)][1] != True:
       graph[(i,j)][0].append((i,j+1))
       graph[(i,j+1)][0].append((i,j))
+      neighbors[(i,j+1)].remove((i,j))
       return (i, j+1)
     else:
       graph[(i,j)][0].append((i-1,j))
       graph[(i-1,j)][0].append((i,j))
+      neighbors[(i-1,j)].remove((i,j))
       return (i-1,j)
   else:
     if graph[(i, j+1)][1] != True:
       graph[(i,j)][0].append((i,j+1))
       graph[(i,j+1)][0].append((i,j))
+      neighbors[(i,j+1)].remove((i,j))
       return (i,j+1)
     else:
       graph[(i,j)][0].append((i,j-1))
       graph[(i,j-1)][0].append((i,j))
+      neighbors[(i,j-1)].remove((i,j))
       return (i,j-1)
 
 #Recursive function that visits every node in a random fashion and
@@ -65,18 +73,40 @@ def visit(graph, neighbors, node, size):
 #This loop executes for every neighbor of node
   while 1:
     if len(neighbors[node]) == 0:
+#If a node has wound up with no neighbors, randomly give it one
+      if len(graph[node][0]) == 0:
+        x,y = node        
+        add = list()
+        if x-1 > 0:
+          add.append((x-1,y))
+        if y-1 > 0:
+          add.append((x,y-1))
+        if x+1 < size:
+          add.append((x+1,y))
+        if y+1 < size:
+          add.append((x,y+1))
+        new =random.choice(add)
+        graph[node][0].append(new)
+        graph[new][0].append(node)
       return
     random.shuffle(neighbors[node])
 #chooses a random node out of the list of nodes neighboring 'node'
     next = neighbors[node][0]
+    neighbors[node].pop(0)
 #if next has been visited already, return
     if graph[next][1] == True:
+      i,j = next
+      if i == size/2 or i == size/2-1:
+        if j == size/2 or j == size/2-1:
+          visit(graph, neighbors, node, size)
+      if len(graph[node][0]) == 0:
+        graph[node][1] = False
+        visit(graph, neighbors, node, size)
       return
 #Make sure that node and next are not checked again as potential
 #neighbors, and connect them by added them into each other's list in 
 #the graph dict   
-    neighbors[node].pop(0)
-    neighbors[next].remove(node)
+    neighbors[next].remove(node)    
     graph[node][0].append(next)
     graph[next][0].append(node)
     visit(graph, neighbors, next, size)
@@ -88,6 +118,12 @@ def make_walls(graph, neighbor, size):
       if False == graph[(i,j)][1]:
         visit(graph, neighbor, (i,j), size)
 
+# def final_check(graph, size)
+  # for i in range(size):
+    # for j in range(size):
+      # if graph[(i,j)]
+
+        
 #When printing out the graph, the very first character is a space
 #Top line alternates between '_' and ' '
 #Every element is checked for a path below the current position and a
@@ -125,6 +161,7 @@ if __name__ == "__main__":
   
   if size < 6 or size%2 == 1:
     print ('Incorrect size. Must be even. Min: 6')
+    exit(1)
 #Neighbor dict is used to hold all possible unvisited neighbors for
 #every node  
   neighbors = defaultdict(list)
